260a30cf6f988afe3eb8ccb301daf7d923448a39
mohammad mohseni <mohammad.mohseni468@gmail.com>
2021-11-09 10:12:12 +0330

    upHandler

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,12 +3,15 @@ import { Container, Button } from "react-bootstrap";
 
 const Counter = () => {
   const [number, setNumber] = useState(0);
+  const upHandler = () => {
+    setNumber(number + 1);
+  };
 
   return (
     <div>
       <Container>
         <h1>{number}</h1>
-        <Button variant="info" onClick={() => setNumber(number + 1)}>
+        <Button variant="info" onClick={upHandler}>
           UP ONE
         </Button>
       </Container>
