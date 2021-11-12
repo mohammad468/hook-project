6623e080e1be8fb3ee4a39e0ebd67d5e29ab06d3
mohammad mohseni <mohammad.mohseni468@gmail.com>
2021-11-12 09:35:11 +0330

    start contexts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Data from "./components/Data";
-import "../node_modules/bootstrap/scss/bootstrap.scss";
+
 
 const App = () => {
   return (
---
+++ src/components/ComA.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ComA = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ComA;
