755573dd057ddc1f9596c4adb9861475a809406a
mohammad mohseni <mohammad.mohseni468@gmail.com>
2021-11-12 11:19:10 +0330

    useReducer

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,55 @@
-import React, {useReducer} from 'react';
+import React, { useReducer } from "react";
+import { Button, Container } from "react-bootstrap";
+
+const initialState = 0;
+
+const reducer = (state, action) => {
+  switch (action) {
+    case "Up":
+      return state + 1;
+    case "Down":
+      return state - 1;
+    case "Reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
 
 const Counter = () => {
-    return (
+  const [number, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div>
+      <Container>
         <div>
-            
+          <h1 className="text-center">{number}</h1>
+          <div className="d-flex justify-content-center">
+            <Button
+              className="mx-2"
+              variant="info"
+              onClick={() => dispatch("Up")}
+            >
+              Up
+            </Button>
+            <Button
+              className="mx-2"
+              variant="warning"
+              onClick={() => dispatch("Reset")}
+            >
+              Reset
+            </Button>
+            <Button
+              className="mx-2"
+              variant="danger"
+              onClick={() => dispatch("Down")}
+            >
+              Down
+            </Button>
+          </div>
         </div>
-    );
+      </Container>
+    </div>
+  );
 };
 
 export default Counter;
